954eb4562c007d9117eb876018fa8de7d4a12e26
CanFam23 <nickclouse03@gmail.com>
2025-08-20 10:32:59 -0600

    Create global exception handler

---
+++ backend/src/main/java/com/it_goes/api/util/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.it_goes.api.util.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import software.amazon.awssdk.http.HttpStatusCode;
+
+import java.util.logging.Logger;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        logger.warning(ex.getMessage());
+        return ResponseEntity.status(HttpStatusCode.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        logger.warning(ex.getMessage());
+        return ResponseEntity.status(HttpStatusCode.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleUnexpected(Exception ex) {
+        logger.warning(ex.getMessage());
+        return ResponseEntity.status(HttpStatusCode.INTERNAL_SERVER_ERROR).body("Something went wrong");
+    }
+}
