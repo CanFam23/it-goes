8e9963e61e93305fb21069594d6280c3ecba9b68
CanFam23 <nickclouse03@gmail.com>
2025-08-22 10:40:46 -0600

    Remove default nextjs content, add custom fonts to rootLayout

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { anonymousPro, inter } from "./ui/fonts";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "../components/Navbar";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +9,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${anonymousPro.variable}`}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="font-inter antialiased bg-[url('/topography.svg')] bg-repeat"
       >
         {children}
       </body>
