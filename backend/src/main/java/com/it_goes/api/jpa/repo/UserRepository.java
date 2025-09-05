13acc77a683676ae8e3936efd23964ec719f7fc0
CanFam23 <nickclouse03@gmail.com>
2025-09-05 10:21:49 -0600

    Add query to user repo to get days skied of each user

--- backend/src/main/java/com/it_goes/api/jpa/repo/UserRepository.java
+++ backend/src/main/java/com/it_goes/api/jpa/repo/UserRepository.java
@@ -1,13 +1,33 @@
 package com.it_goes.api.jpa.repo;
 
+import com.it_goes.api.jpa.projection.FirstNameDays;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.it_goes.api.jpa.model.User;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    /**
+     * Gets the number of days each user has skied between the start and end date
+     * @param start Start date
+     * @param end End date
+     * @return List of {@link FirstNameDays} projections with the numbers of days skied and the first name of the user
+     */
+    @Query(value = """
+     SELECT u.first_name, COUNT(*) AS days_skied FROM trip_users 
+     JOIN trip as t ON trip_users.trip_id = t.id 
+     JOIN it_goes_user as u ON trip_users.user_id = u.id 
+     WHERE t.date_of_trip >= :start and t.date_of_trip <= :end
+     GROUP BY u.id,u.first_name;
+    """, nativeQuery = true)
+    List<FirstNameDays> getDaysSkied(@Param("start") LocalDate start, @Param("end") LocalDate end);
 }
