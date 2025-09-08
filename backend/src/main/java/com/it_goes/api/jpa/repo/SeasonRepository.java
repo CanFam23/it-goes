a65fe35ebb5aceb3be10197cf44a5336ff9b1259
CanFam23 <nickclouse03@gmail.com>
2025-09-08 09:36:59 -0600

    Create method in seasonRepo to get season by year

--- backend/src/main/java/com/it_goes/api/jpa/repo/SeasonRepository.java
+++ backend/src/main/java/com/it_goes/api/jpa/repo/SeasonRepository.java
@@ -2,6 +2,16 @@ package com.it_goes.api.jpa.repo;
 
 import com.it_goes.api.jpa.model.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface SeasonRepository extends JpaRepository<Season, Long> {
+
+    @Query(value = """
+            SELECT * from season
+            WHERE EXTRACT(year FROM start_date) = :year
+            """, nativeQuery=true)
+    Optional<Season> findByStartYear(@Param("year") int year);
 }
