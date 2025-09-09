c42ae264091816a99693f93c8f39bb7a7f5d431f
CanFam23 <nickclouse03@gmail.com>
2025-09-09 09:45:52 -0600

    Add trip entries for second year in data seeder

--- backend/src/main/java/com/it_goes/api/config/DataSeeder.java
+++ backend/src/main/java/com/it_goes/api/config/DataSeeder.java
@@ -35,7 +35,10 @@ public class DataSeeder {
 
         final Season season = new Season(2024);
 
+        final Season season2 = new Season(2023);
+
         sr.save(season);
+        sr.save(season2);
 
         ur.save(newUser);
         lr.save(location);
@@ -47,11 +50,24 @@ public class DataSeeder {
         ur.save(newUser3);
 
         for(int i = 0; i < 8; i++){
-            final Trip t = new Trip("4th of July Couloir " + i, newUser, location, LocalDate.of(2025,6,i + 1));
+            Trip t = new Trip("4th of July Couloir " + i, newUser, location, LocalDate.of(2025,6,i + 1));
             t.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
             season.addTrip(t);
             t.setSeason(season);
+            t.setCoverImageKey("images/DSC02993.jpg");
+
+            newUser.addTripPosted(t);
+            newUser2.addTrip(t);
+            newUser3.addTrip(t);
+
+            tr.save(t);
+
+            t = new Trip("4th of July Couloir " + i, newUser, location, LocalDate.of(2024,6,i + 1));
+            t.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+            season2.addTrip(t);
+            t.setSeason(season2);
             t.setCoverImageKey("images/DSC02993.jpg");
 
             newUser.addTripPosted(t);
