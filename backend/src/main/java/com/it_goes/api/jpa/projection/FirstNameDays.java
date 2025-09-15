ca367ee8914f1fb04422fe67fed0e14af6d69b63
CanFam23 <nickclouse03@gmail.com>
2025-09-14 20:35:17 -0600

    Add projection for first name, days, location, and year

--- backend/src/main/java/com/it_goes/api/jpa/projection/FirstNameDays.java
+++
@@ -1,7 +0,0 @@
-package com.it_goes.api.jpa.projection;
-
-public interface FirstNameDays {
-    String getFirstName();
-
-    int getDaysSkied();
-}
---
+++ backend/src/main/java/com/it_goes/api/jpa/projection/FirstNameDaysLocationYear.java
@@ -0,0 +1,11 @@
+package com.it_goes.api.jpa.projection;
+
+public interface FirstNameDaysLocationYear {
+    String getFirstName();
+
+    String getLocation();
+
+    int getDaysSkied();
+
+    Integer getYear();
+}
