8194594d9648eb134c94f3626578ec7abf2fae0e
CanFam23 <nickclouse03@gmail.com>
2025-09-14 18:53:36 -0600

    Update how trips component functions when no trips are given, add margin to error msg class

--- frontend/src/components/Trips.js
+++ frontend/src/components/Trips.js
@@ -1,7 +1,11 @@
 import {headers} from "next/headers";
 import TripSummaryCard from "@/components/TripSummaryCard";
 
-export default async function Trips({trips}){
+export default function Trips({trips}){
+  if (!trips || trips.length === 0){
+    return <p className="error-msg">No recent trips found</p>
+  }
+
   return (
     <div className="w-full">
       {Object.entries(trips).map(([key, trip]) => (
