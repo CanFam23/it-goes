003b878356b4f39a502b44856c48e872f244d977
CanFam23 <nickclouse03@gmail.com>
2025-09-14 20:39:19 -0600

    Add new jpa method to get years skied for each user at each location

--- backend/src/main/java/com/it_goes/api/jpa/repo/UserRepository.java
+++ backend/src/main/java/com/it_goes/api/jpa/repo/UserRepository.java
@@ -1,6 +1,6 @@
 package com.it_goes.api.jpa.repo;
 
-import com.it_goes.api.jpa.projection.FirstNameDays;
+import com.it_goes.api.jpa.projection.FirstNameDaysLocationYear;
 import com.it_goes.api.jpa.projection.FirstNameDaysYear;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -27,24 +27,43 @@ public interface UserRepository extends JpaRepository<User, Long> {
      SELECT u.first_name, COUNT(*) AS days_skied,
      :start AS startDate,
      EXTRACT(YEAR FROM CAST(:start AS DATE)) AS year
-     FROM trip_users 
-     JOIN trip as t ON trip_users.trip_id = t.id 
-     JOIN it_goes_user as u ON trip_users.user_id = u.id 
+     FROM trip_users
+     JOIN trip as t ON trip_users.trip_id = t.id
+     JOIN it_goes_user as u ON trip_users.user_id = u.id
      WHERE t.date_of_trip >= :start and t.date_of_trip <= :end
      GROUP BY u.id,u.first_name;
     """, nativeQuery = true)
     List<FirstNameDaysYear> getDaysSkied(@Param("start") LocalDate start, @Param("end") LocalDate end);
 
+    /**
+     * Gets the number of days each user has skied between the start and end date at every location in the database
+     * @param start Start date
+     * @param end End date
+     * @return List of {@link FirstNameDaysLocationYear} projections with the numbers of days skied, the first name of the user,
+     * the location of each trip, the year found in the start variable given
+     */
+    @Query(value = """
+      SELECT u.first_name, l.name, COUNT(*) AS days_skied,
+      :start AS startDate,
+      EXTRACT(YEAR FROM CAST(:start AS DATE)) AS year
+      FROM trip_users
+      JOIN trip AS t ON trip_users.trip_id = t.id
+      JOIN it_goes_user AS u ON trip_users.user_id = u.id
+      JOIN location AS l on t.location_id = l.id
+      WHERE t.date_of_trip >= :start and t.date_of_trip <= :end
+      GROUP BY u.id,u.first_name,l.name, s.start_date;
+    """, nativeQuery = true)
+    List<FirstNameDaysLocationYear> getDaysSkiedEachLocation(@Param("start") LocalDate start, @Param("end") LocalDate end);
 
     /**
      * Gets the number of days each user has skied for each season in the database
      * @return List of {@link FirstNameDaysYear} projections with the numbers of days skied, the first name of the user, and the year
      */
     @Query(value = """
-     SELECT u.first_name, COUNT(*) AS days_skied, EXTRACT(year FROM s.start_date) as year 
-     FROM trip_users 
-     JOIN trip as t ON trip_users.trip_id = t.id 
-     JOIN it_goes_user as u ON trip_users.user_id = u.id 
+     SELECT u.first_name, COUNT(*) AS days_skied, EXTRACT(year FROM s.start_date) as year
+     FROM trip_users
+     JOIN trip as t ON trip_users.trip_id = t.id
+     JOIN it_goes_user as u ON trip_users.user_id = u.id
      JOIN season as s ON t.season_id = s.id
     GROUP BY u.id,u.first_name, s.start_date;
     """, nativeQuery = true)
