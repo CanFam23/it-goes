d95a4f4f6de26dfdda26940ed3b2027f0106dc63
CanFam23 <nickclouse03@gmail.com>
2025-09-14 20:36:05 -0600

    Add new location to data seeder

--- backend/src/main/java/com/it_goes/api/config/DataSeeder.java
+++ backend/src/main/java/com/it_goes/api/config/DataSeeder.java
@@ -33,6 +33,8 @@ public class DataSeeder {
 
         final Location location = new Location("Beehive Basin", State.MT, Country.US);
 
+        final Location location2 = new Location("Glacier National Park", State.MT, Country.US);
+
         final Season season = new Season(2024);
 
         final Season season2 = new Season(2023);
@@ -42,6 +44,7 @@ public class DataSeeder {
 
         ur.save(newUser);
         lr.save(location);
+        lr.save(location2);
 
         final User newUser2 = new User("cmar","connor@gmail.com", "password","Connor","Marland",null);
         final User newUser3 = new User("jswea","jake@gmail.com", "password","Jake","Sweatland",null);
@@ -50,7 +53,7 @@ public class DataSeeder {
         ur.save(newUser3);
 
         for(int i = 0; i < 8; i++){
-            Trip t = new Trip("4th of July Couloir " + i, newUser, location, LocalDate.of(2025,6,i + 1));
+            Trip t = new Trip("4th of July Couloir " + i, newUser, i % 2 == 0 ? location : location2, LocalDate.of(2025,6,i + 1));
             t.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
             season.addTrip(t);
@@ -63,7 +66,7 @@ public class DataSeeder {
 
             tr.save(t);
 
-            t = new Trip("4th of July Couloir " + i, newUser, location, LocalDate.of(2024,6,i + 1));
+            t = new Trip("4th of July Couloir " + i, newUser, i % 2 == 0 ? location : location2, LocalDate.of(2024,6,i + 1));
             t.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
             season2.addTrip(t);
