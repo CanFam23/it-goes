46d07e8966131a5dfd1cc866db77613ec89e212a
CanFam23 <nickclouse03@gmail.com>
2025-09-17 21:28:21 -0600

    Add method in trip service to convert gpx data to lineString

--- backend/src/main/java/com/it_goes/api/service/TripService.java
+++ backend/src/main/java/com/it_goes/api/service/TripService.java
@@ -5,11 +5,21 @@ import com.it_goes.api.dto.TripSummaryDto;
 import com.it_goes.api.jpa.model.Trip;
 import com.it_goes.api.jpa.model.User;
 import com.it_goes.api.util.exception.NotFoundException;
+import io.jenetics.jpx.GPX;
+import io.jenetics.jpx.Track;
+import io.jenetics.jpx.TrackSegment;
+import org.locationtech.jts.geom.*;
 import org.springframework.data.domain.Page;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.NoSuchElementException;
 
 public interface TripService {
     int MIN_NUM_TRIPS = 1;
     int MAX_NUM_TRIPS = 5;
+
+    GeometryFactory gf = new GeometryFactory(new PrecisionModel(), 4326);
 
     /**
      * Given a {@link Trip} object, converts it to a {@link TripSummaryDto}.
@@ -34,6 +44,43 @@ public interface TripService {
                 trip.getDatePosted(), trip.getDateUpdated(),
                 trip.getRoute(), trip.getAuthor().getUsername(),
                 trip.getUsers().stream().map(User::getUsername).toArray(String[]::new));
+    }
+
+    /**
+     * Given a {@link Path} of a .gpx file, attempts to convert the gpx file into a {@link LineString} object.
+     * @param p Path of gpx file.
+     * @return The created LineString object, or null if an error occurs or the file is not a gpx file.
+     */
+    static LineString toLineString(Path p){
+        // Only convert gpx files to lineString
+        if (!p.getFileName().toString().endsWith(".gpx")){
+            return null;
+        }
+
+        try {
+            final GPX gpx = GPX.read(p);
+            final Track track = gpx.getTracks().getFirst();
+
+            final TrackSegment segment = track.getSegments().getFirst();
+
+            final Coordinate[] coords = segment.points()
+                    .map(point -> {
+                        if (point.getElevation().isEmpty() || point.getTime().isEmpty()) {
+                            return null;
+                        }
+
+                        return new CoordinateXYZM(
+                                point.getLongitude().doubleValue(),   // X
+                                point.getLatitude().doubleValue(),    // Y
+                                point.getElevation().get().doubleValue(), // Z
+                                point.getTime().get().getEpochSecond()); // M / Time
+                    })
+                    .toArray(Coordinate[]::new);
+
+            return gf.createLineString(coords);
+        } catch (IOException | NullPointerException | NoSuchElementException e) {
+            return null;
+        }
     }
 
     /**
