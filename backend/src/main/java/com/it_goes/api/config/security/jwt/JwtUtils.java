4a379b137ecad22d05dc418c071a7d073b4e5f44
CanFam23 <nickclouse03@gmail.com>
2025-11-21 15:03:49 -0700

    Add JWT config files

---
+++ backend/src/main/java/com/it_goes/api/config/security/jwt/JwtUtils.java
@@ -0,0 +1,63 @@
+package com.it_goes.api.config.security.jwt;
+
+import com.it_goes.api.config.security.user.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        final UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token");
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired");
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty");
+        }
+
+        return false;
+    }
+}
